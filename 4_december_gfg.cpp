2afe7e6cb906a7b833a3bb75f67154210acfa5f8
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-10-18 16:14:14 +0530

    Create 4_december_gfg.cpp

---
+++ 4_december_gfg.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    string rearrange(string S, int N)
+    {
+        // your code here
+      
+    }
+};
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.rearrange(s, n) << endl;
+    }
+}
